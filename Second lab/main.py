a29bc1caa16d3b7a356c29d0de2cfcc6a48e7289
Асламов Артем <aslamov@promsoft.ru>
2021-10-18 23:33:43 +0700

    56+2

--- Second lab/main.py
+++ Second lab/main.py
@@ -1,27 +1,50 @@
-class TreeNode:
-    def __init__(self):
+class Node:
+    def __init__(self, key, value=None):
+        self.key = key
+        self.value = value
         self.children = list()
 
-    def insert(self, key, value=None):
-        self.key = key
 
-        if len(self.key) == 0:
-            self.value = value
+
+class Tree:
+    def __init__(self):
+        self.root = Node(' ')
+
+    def insert(self, key, parent=None, value=None):
+
+        if key:
             return
-        letter = self.key[0]
-        if letter in self.children:
-            self.insert(self.key[1:], value)
-        else:
-            self.children.append(letter)
-            self.insert(self.key[1:], value)
 
-    # def lookup(self):
+        elif len(key) == 1:
+            new_node = Node(key, value)
+            parent.children.append(new_node)
+            print(parent.children.append(new_node))
+            return
+        # letter = key[0]
+        # if letter in parent.children:
+        #     new_node = Node(key, value)
+        #     node.insert(self.key[1:], value)
+        # else:
+        #     self.children.append(letter)
+        #     print(self.children)
+        #     print("---" * 100)
+        #     node = Node()
+        #     node.insert(self.key[1:], value)
+
+    # def lookup(self, key):
+    #     if len(key) == 1:
+    #         return self.value
+    #     else:
+    #         letter = self.key[0]
 
 
-a = TreeNode()
-a.insert('artem', 213)
-a.insert('hjwdbchjbwdhbchjwdjchjwvcgvgvvdvwgcvtywrvurwvfyubewhjdyuwgjwjvwrjbvhjrgvbefvbyrbcb', 3746374)
-print(a.children)
-print(a.value)
+
+
+
+
+a = Tree()
+a.insert('a', 213)
+a.insert('hjwdbchjbwdhbchjwdjchjwvcgvgvvdvwgcvtywrvurwvfyubewhjdyuwgjwjvwrjbvhjrgvbefvbyrbcb8888888888888888', 3746374)
+# print(a.lookup('a'))
 
 
